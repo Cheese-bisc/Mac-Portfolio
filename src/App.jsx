b08d44be8ebadb2b8e311db9bf2ea197e163b582
Aryan Khaund <aryan.khaund@gmail.com>
2026-06-25 02:10:49 +0530

    Implement Welcome Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// import { Navbar, Welcome } from "#components/index";
 import Navbar from "#components/Navbar";
+import Welcome from "#components/Welcome";
 import React from "react";
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <Welcome />
     </main>
   );
 };
